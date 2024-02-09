e9c2232b3750b6358440ad178746f956f7dc0021
Riptide 776 <125511300+Matt-Deg@users.noreply.github.com>
2024-02-09 00:01:16 -0500

    Update SearchEngine.java
    Fixed formatting issues

--- SearchEngine.java
+++ SearchEngine.java
@@ -15,24 +15,24 @@ import com.mongodb.client.MongoCollection;
 //implements the UserInteractions interface
 public class SearchEngine implements UserInteractions {
 	
-	private static final String START_URL = "https://www.muhlenberg.edu/";  //where to begin crawling
-	private static final String CrawlerInventoryCollectionName = "CrawlerDocs"; //MongoDB collection where crawled docs are stored
-	private static final String InvertedIndexCollection = "InvertedIndex"; //MongoDB collection where inverted index docs are stored
+    private static final String START_URL = "https://www.muhlenberg.edu/";  //where to begin crawling
+    private static final String CrawlerInventoryCollectionName = "CrawlerDocs"; //MongoDB collection where crawled docs are stored
+    private static final String InvertedIndexCollection = "InvertedIndex"; //MongoDB collection where inverted index docs are stored
 	
-	private static String dbURI; //MongoDB connection URI - input from user in GUI
-	private static String dbNAME; //MongoDB database to connect to - input from user in GUI
+    private static String dbURI; //MongoDB connection URI - input from user in GUI
+    private static String dbNAME; //MongoDB database to connect to - input from user in GUI
 	
-	private GUI gui; //a GUI object for the search engine
-	private static mongoConnect mongoDB; //to be used to connect to MongoDB when crawler is used
-	private static Crawler crawler; //a Crawler object for the search engine
-	private BrowserAlgorithm browser; //a BrowserAlgorithm object so search engine can access the search algorithm
+    private GUI gui; //a GUI object for the search engine
+    private static mongoConnect mongoDB; //to be used to connect to MongoDB when crawler is used
+    private static Crawler crawler; //a Crawler object for the search engine
+    private BrowserAlgorithm browser; //a BrowserAlgorithm object so search engine can access the search algorithm
 	
-	private boolean crawlerUsed = false; //if crawler is used, signal to callBrowser() that dbURI and dbNAME still require initialization
+    private boolean crawlerUsed = false; //if crawler is used, signal to callBrowser() that dbURI and dbNAME still require initialization
 
-	/**
-	 * Main method that creates and initializes a search engine object
-	 * When search engine initialized, GUI constructs and brings up welcome screen
-	 */
+    /**
+     * Main method that creates and initializes a search engine object
+     * When search engine initialized, GUI constructs and brings up welcome screen
+    */
     public static void main(String[] args) {
     	SearchEngine se = new SearchEngine();
         se.initialize();
@@ -62,7 +62,7 @@ public class SearchEngine implements UserInteractions {
      * Connect and setup MongoDB database for this project
      * Initialize crawler and boot up crawling service
      * @param link The database URI input from the user
-	 * @param dbName The name of the database where crawled data will be stored, taken from user input
+     * @param dbName The name of the database where crawled data will be stored, taken from user input
      */
     public void crawlSelected (String link, String dbName) {
     	
@@ -98,20 +98,21 @@ public class SearchEngine implements UserInteractions {
      * Initializes the browser
      * Opens browser GUI display
      * @param link The database URI input from the user
-	 * @param dbName The name of the database where crawled data will be stored, taken from user input
+     * @param dbName The name of the database where crawled data will be stored, taken from user input
      */
     public void callBrowser(String link, String dbName) {
     	
     	//closes crawler connection if it existed
     	if(mongoDB != null) {
-    		mongoDB.closeConnection();
+    	    mongoDB.closeConnection();
     	}
     	
     	//initialize class variables if not done already through crawler
     	if(!crawlerUsed) {
-    		dbURI = link;
-        	dbNAME = dbName;
+    	    dbURI = link;
+            dbNAME = dbName;
     	}
+
     	browser = new BrowserAlgorithm(CrawlerInventoryCollectionName, InvertedIndexCollection, dbURI, dbNAME);
     	gui.browserScreen();
     }
